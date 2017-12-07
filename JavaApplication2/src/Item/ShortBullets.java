923e9799ccc5d864fccb925f33afc3a4ae83a5a1
pauloneves123 <josepaulobrandaoneves13@gmail.com>
2017-12-07 17:51:58 -0300

    short bullets
    FALTA COLOCAR REMOVER ITENS QUANDO USAR ALGUM ITEM, FALTA COLOCAR
    FUNÇÕES DE ATAQUE DIFERENTES EM INTERFACE FALTA FAZER AS INTERAÇÕES DE
    GUNS COM AMMUNITION, FALTA MEXER NOS AMBIENTES, FALTA COLOCAR RANDOM NA
    DECLARAÇÃO DE MONSTROS NOS AMBIENTES, FALTA CRIAR UMA INTERFACE MENU
    PARA O PERSONAGEM.

---
+++ JavaApplication2/src/Item/ShortBullets.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Item;
+
+/**
+ *
+ * @author 201604940046
+ */
+public class ShortBullets {
+    //BALAS DE PISTOLA
+}
